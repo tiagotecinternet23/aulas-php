b2ac19305ac66602ea5f3dd60930f31ba953ff40
Tiago B. dos Santos <tiago.tecinternet23@gmail.com>
2023-08-03 09:01:58 -0300

    Função com return

--- 06-funcoes.php
+++ 06-funcoes.php
@@ -30,6 +30,17 @@ function dadosAutor(){
     </article>
 </section>
 
+<hr>
+
+<h2>Função com retorno de dados</h2>
+<?php
+function dadosCurso(){
+    // return "Técnico em Informática para Internet";
+    return "<b>Encher o saco no WhatsApp</b>";
+}
+?>
+<p>Estamos no curso de <?=dadosCurso()?></p>
+<p><?=dadosCurso()?> é ministrado no Senac Penha.</p>
 
 </body>
 </html>

c4b4371228bd833ab64a5631f80964e2e2c6942e
Tiago B. dos Santos <tiago.tecinternet23@gmail.com>
2023-08-04 11:02:35 -0300

    trim()

---
+++ 07-funcoes-nativas.php
@@ -0,0 +1,33 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Funções nativas</title>
+</head>
+<body>
+    <h1>Exemplos de funções nativas</h1>
+    <hr>
+
+    <h2>Strings</h2>
+    <h3>trim()</h3>
+<?php
+/* Função que remove espaços
+antes e depois de strings. */
+$nome = "   Vinicius me deve 80 paçocas   ";
+$nomeSemEspaco = trim($nome);
+?>
+<pre> <?=var_dump($nome)?> </pre>
+<pre> <?=var_dump($nomeSemEspaco)?> </pre>
+
+    <hr>
+    <h2>Numéricas</h2>
+    
+    <h2>Arrays</h2>
+    
+    <h2>Filtros</h2>
+    
+    <h2>Segurança</h2>
+
+</body>
+</html>

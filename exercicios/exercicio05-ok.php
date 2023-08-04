c3c1d6cd39944699b2dbc51ca6fd837f660e7bff
Tiago B. dos Santos <tiago.tecinternet23@gmail.com>
2023-08-04 10:20:56 -0300

    Aplicação de estilos/classes

--- exercicios/exercicio05-ok.php
+++ exercicios/exercicio05-ok.php
@@ -4,7 +4,10 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Exercício 05 - Funções (OK)</title>
-
+<style>
+    .aprovado { color: blue; }
+    .reprovado { color: red; }
+</style>
 </head>
 <body>
     <h1>Exercício 05 - Funções (OK)</h1>
@@ -34,7 +37,7 @@ $situacao = verificarSituacao($media);
 
 ?>
 <p> <?=$media?> </p>
-<p> <?=$situacao?> </p>
+<p class="<?=$situacao?>"> <?=$situacao?> </p>
   
 
 

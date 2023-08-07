0d24eb7e9b4dbf2a18111fb775aace6f721403ba
Tiago B. dos Santos <tiago.tecinternet23@gmail.com>
2023-08-07 09:36:04 -0300

    Algoritmos e funções pra criptografia

--- 07-funcoes-nativas.php
+++ 07-funcoes-nativas.php
@@ -149,7 +149,45 @@ echo $ataqueSanitizado;
 
 
     <hr>
-    <h2>Segurança</h2>
+
+    <h2>Segurança (criptografia de dados)</h2>
+<?php
+// Senha em texto puro (plain text)
+$senha = "123senac";
+
+/* Algoritmos mais comuns para criptografia:
+MD5, SHA1, SHA256 
+
+Estes algoritmos pegam os dados e os codificam/criptografam,
+tornando um "hash" de dados embaralhados. */
+$senhaMD5 = md5($senha);
+$senhaSHA1 = sha1($senha);
+$senhaSHA256 = hash('sha256', $senha);
+?>
+<p>Senha (texto puro): <?=$senha?></p>
+<p>Senha (MD5): <?=$senhaMD5?></p>
+<p>Senha (SHA1): <?=$senhaSHA1?></p>
+<p>Senha (SHA256): <?=$senhaSHA256?></p>
+
+<?php
+/* Método recomendado para segurança de senhas
+no PHP. */
+$senhaSegura = password_hash($senha, PASSWORD_DEFAULT);
+?>
+<p>Senha (com password_hash): <?=$senhaSegura?></p>
+
+<?php
+/* Como verificar o hash da senha se ele pode mudar? */
+$senhaDigitada = "123senacc";
+
+// if ( $senhaDigitada === $senhaSegura ) { // NÃO DÁ CERTO!
+if( password_verify($senhaDigitada, $senhaSegura) ){    
+    echo "senha correta, pode entrar...";
+} else {
+    echo "senha errada, vaza daqui disgraça!";
+}
+
+?>
 
 </body>
 </html>

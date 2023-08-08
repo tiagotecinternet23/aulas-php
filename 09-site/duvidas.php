5f77bcfdd0a59cb689b0385d6e0237f4583d017c
Tiago B. dos Santos <tiago.tecinternet23@gmail.com>
2023-08-08 09:00:33 -0300

    Solução 1 para os títulos
    Usando variáveis para cada página

--- 09-site/duvidas.php
+++ 09-site/duvidas.php
@@ -1,4 +1,6 @@
-<?php require "includes/cabecalho.php" ?>
+<?php 
+$titulo = "Dúvidas";
+require "includes/cabecalho.php" ?>
         <h2>Página de dúvidas</h2>
         <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Esse fugit, aperiam sapiente commodi quibusdam dolore?</p>
         <h3>Principais dúvidas</h3>

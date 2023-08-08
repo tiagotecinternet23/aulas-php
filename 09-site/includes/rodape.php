51446bfb56ff90e19969a071675a23ad0e85ec7d
Tiago B. dos Santos <tiago.tecinternet23@gmail.com>
2023-08-08 08:55:28 -0300

    Script de data/hora no rodapé

--- 09-site/includes/rodape.php
+++ 09-site/includes/rodape.php
@@ -1,7 +1,13 @@
 </main>
-    
-    <footer>
-        <p>Site criado para o curso <b>Téc. Informática para Internet</b></p>
-    </footer>
+
+<?php
+date_default_timezone_set("America/Sao_Paulo");
+$dataHoraAtual = date("d/m/Y h:i");
+//echo $dataHoraAtual;
+?>
+
+<footer>
+    <p>Site criado para o curso <b>Téc. Informática para Internet</b> - <time><?=$dataHoraAtual?></time> </p>
+</footer>
 </body>
 </html>

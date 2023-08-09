e1fdedc1961be3eec5d5e07babc3e56b6ca4329a
Tiago B. dos Santos <tiago.tecinternet23@gmail.com>
2023-08-09 08:40:01 -0300

    Revert "Solução exercício interesses"
    This reverts commit 31e0a4569ec6f2dfdaf3dd5ef6c315576447d961.

--- processa-post.php
+++ processa-post.php
@@ -28,15 +28,7 @@ if( empty($_POST["nome"]) || empty($_POST["email"]) ){
     /* Se houver interesses (ou seja, foi selecionado
     pelo menos 1), guarde na variável o $_POST["interesses"].
     Caso contrário, guarde na variável um array vazio. */
-    //$interesses = $_POST["interesses"];
-
-    /* Solução do desafio */
-    if (isset($_POST['interesses'])) {
-        $interesses = $_POST['interesses'];
-    } else {
-        $interesses = []; // array()
-    }    
-
+    $interesses = $_POST["interesses"];
 ?>
     <h2>Dados:</h2>
     <ul>
